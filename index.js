7b6fb56114dba03967b7c076a6e8c8ef53fe343e
Rajak90 <rajashekar.k7@gmail.com>
2024-03-09 15:52:01 +0530

    created user crud api with MongoDB

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./db');
+const routes = require('./routes');
+
+const app = express();
+
+// app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+
+app.use('/', routes);
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
